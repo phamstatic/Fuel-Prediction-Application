612d79cded14b09ebacc0e17a920aab951ba0b69
root <root@DESKTOP-TEDCC3M>
2023-09-20 12:48:02 -0500

    Converted Anthony's Profile page from HTML to React JSX.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                     <Route path='/' element={<Welcome/>}/>
                     <Route path='/Login' element={<Login/>}/>
                     <Route path='/Register' element={<Register/>}/>
+                    <Route path='/Profile' element={<Profile/>}/>
                 </Routes>
             </Router>
 
