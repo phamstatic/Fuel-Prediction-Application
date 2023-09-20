aecc2658ca12bd83d61453250449db8902adf716
root <root@DESKTOP-TEDCC3M>
2023-09-20 13:07:54 -0500

    Setting up the Fuel Quote page.

---
+++ front-end/src/pages/Quote.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../components/home/Home.css";
+
+const Quote = () => {
+    return (
+        <>
+            <div class="container">
+
+            </div>
+        </>
+    );
+}
+
+export default Quote;
