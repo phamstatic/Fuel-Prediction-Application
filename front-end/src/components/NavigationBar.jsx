b37586514cbbfe238ace532c1c9bdb3a8e118ba5
root <root@DESKTOP-TEDCC3M>
2023-09-22 19:24:05 -0500

    Fixed background image from repeating.

--- front-end/src/components/NavigationBar.jsx
+++ front-end/src/components/NavigationBar.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "../stylesheets/Navigation.css";
-import { NavLink as Link } from "react-router-dom";
 
 const NavigationBar = () => {
     return (
         <>
             <div class = "navigationBar">
-                <img className="logo" src="https://uh.edu/brand/_img/uh_white_black.png"/>
+                <img className="logo" src="https://uh.edu/brand/_img/uh_white_black.png" alt="UH Logo"/>
                 <div class="apptitle">Fuel Prediction Application</div>
             </div>
         </>
