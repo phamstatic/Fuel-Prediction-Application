2c28ddda9455593a472a09689361c5db08ec01b8
jdoggram <j.d.graham.00@outlook.com>
2023-10-10 21:28:12 -0500

    Testing Branch
    Figuring out backend

--- back-end/server.js
+++ back-end/server.js
@@ -13,3 +13,17 @@ app.get('/message', (req, res) => {
 app.listen(8000, () => {
     console.log(`Server is running on port 8000.`);
   });
+
+history = {
+    1: {
+        fuelAmount: 5,
+        profitMargin: 0.125,
+        totalCost: 65403.12,
+        address: "32 P. Sherman, Wallaby Way, Sydney AU",
+        date: "11/5/2015"
+    },
+  }
+
+  app.get('/history', (req, res) => {
+    res.json({ history: history });
+});
