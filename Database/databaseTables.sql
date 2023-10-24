9cb372509498f49eb72944c10ea3c0213c9e9b22
Alexander Catchings <hieuviet0711@gmail.com>
2023-10-24 18:13:20 -0500

    db tables add

---
+++ Database/databaseTables.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS "login";
+CREATE TABLE "login"(
+    "username" VARCHAR PRIMARY KEY,
+    "password" varchar
+);
+
+DROP TABLE IF EXISTS "client";
+CREATE TABLE "client"(
+    "username" VARCHAR PRIMARY KEY,
+    "fullName" VARCHAR,
+    "address1" VARCHAR,
+    "address2" VARCHAR,
+    "city" VARCHAR,
+    "State" VARCHAR(2),
+    "zip" VARCHAR
+)
+
+DROP TABLE IF EXISTS "fuelQuote";
+CREATE TABLE "fuelQuote"(
+    "username" Varchar PRIMARY KEY,
+    "requested" INT,
+    "delivAddress" VARCHAR, 
+    "delivDate" VARCHAR,
+    "suggPrice" DECIMAL(10,2)
+)
---
+++ Database/insertLogin.sql
@@ -0,0 +1,5 @@
+INSERT INTO login (username, password)
+VALUES('admin', 'password');
+
+INSERT INTO login (username, password)
+VALUES('user1', 'password12345');
