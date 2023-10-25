e3ae7ce3ff20f90b4ea425f1b51a8614cd19a16e
John Pham <phambinouh@gmail.com>
2023-10-25 18:16:18 -0500

    Successful initial verification.

--- back-end/routes/Register.js
+++ back-end/routes/Register.js
@@ -6,23 +6,47 @@ router.get('/', async(req, res) => {
     res.send("GET HANDLER for /REGISTER route");
 })
 
-// Need a way to add user login information to save and verify.
-router.post('/', async (req, res) => { 
-    let user = req.body;
-    console.log(user);
-    console.log(`User ${user.username} registered with password ${user.password}`);
-    res.status(200).send(`User ${user.username} registered with password ${user.password}`);
+PrintAll = () => {
+    let sql = `SELECT * FROM login;`;
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+}
 
-
+AddUser = (username, password) => {
     let sql = 
     `
     INSERT INTO login (username, password)
-    VALUES('${user.username}', '${user.password}');
+    VALUES('${username}', '${password}');
     `;
     connection.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log(result);
+        if (err) throw err;
+        PrintAll();
     });
+}
+
+router.post('/', async (req, res) => {
+    let user = req.body;
+    try {
+        connection.query(
+            `SELECT 1 FROM login WHERE username = '${user.username}';`,
+            (err, result) => {
+                if (err) throw err;
+                else if (result.length > 0) { // The username is taken
+                    console.log("username taken");
+                }
+                else { // The username is not taken
+                    console.log("username not taken");
+                    AddUser(user.username, user.password);
+                }
+            }
+        )
+    }
+    catch (error) {
+         
+    }
+
 });
 
 module.exports = router;
