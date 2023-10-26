fa28c6c8f4983ae2d7542f54bb05d38f39acda71
John Pham <phambinouh@gmail.com>
2023-10-25 23:25:31 -0500

    Removed folder.

--- Database/insertQuote.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO fuelQuote(username,requested,delivAddress,delivDate,suggPrice)
-VALUES('admin',20,'address1','October 24th 2023', 5.99)
-
-INSERT INTO fuelQuote(username,requested,delivAddress,delivDate,suggPrice)
-VALUES('user1',15,'Bind blvd','August 1st 2023', 8)
