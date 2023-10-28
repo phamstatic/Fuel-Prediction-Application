270a64f9fdde3d6d61b11809e84c097871dca197
John Pham <phambinouh@gmail.com>
2023-10-27 22:17:24 -0500

    100% coverage for Fuel Quote Module route

--- back-end/routes/FuelQuoteModule.js
+++ back-end/routes/FuelQuoteModule.js
@@ -3,40 +3,42 @@ var router = express.Router();
 var connection = require("../database/Database");
 
 class FuelQuoteModule {
-    constructor(gallonsRequested, deliveryAddress, deliveryDate) {
+    constructor(username, gallonsRequested, deliveryAddress, deliveryDate) {
+        this.username = username;
         this.gallonsRequested = gallonsRequested;
         this.deliveryAddress = deliveryAddress;
         this.deliveryDate = deliveryDate;
-        this.suggestedPrice = 0; 
+        this.suggestedPrice = 0;
     }
     createQuote() {
-        this.suggestedPrice = 0;
+        this.suggestedPrice = 0.00;
         return this.suggestedPrice;
     }
 }
 
-router.get('/', async(req, res) => {
-    res.status(200).send(quote.createQuote);
-
+router.get('/', async (req, res) => {
+    res.send('GET HANDLER for /PROFILE/QUOTE route');
 })
 
-router.post('/', async (req, res) => { 
+router.post('/', async (req, res) => {
     let user = req.body;
-    console.log(user);
-    const quote = new FuelQuoteModule(user.gallonsRequested, user.deliveryAddress, user.deliveryDate);
+    const quote = new FuelQuoteModule(user.username, user.gallonsRequested, user.deliveryAddress, user.deliveryDate);
     let sugg = quote.createQuote();
 
-    let sql = 
+    let sql =
     `
-    INSERT INTO fuelquote(requested, delivAddress,delivDate,suggPrice)
-    VALUES( '${user.gallonsRequested}','${user.deliveryAddress}','${user.deliveryDate}','${sugg}');
+    INSERT INTO fuelquote(username, requested, delivAddress, delivDate, suggPrice)
+    VALUES('${user.username}', '${user.gallonsRequested}','${user.deliveryAddress}','${user.deliveryDate}','${sugg}');
     `;
-
-    connection.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
+    try {
+        connection.query(sql, (err, result) => {
+            res.status(200).send({
+                message: "Fuel quote information successfully quoted!"
+            });
         });
-    
+    }
+    catch (error) {
+    }
 });
 
 module.exports = router;
