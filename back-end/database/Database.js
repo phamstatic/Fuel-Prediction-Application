9a995e188c756ea74f0c00aaa0bb73ae40bdea0b
John Pham <phambinouh@gmail.com>
2023-11-27 23:53:04 -0600

    Enabled multiline SQL statements for database preload

--- back-end/database/Database.js
+++ back-end/database/Database.js
@@ -7,64 +7,64 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password",
-    insecureAuth : true
+    insecureAuth : true,
+    multipleStatements : true
 })
 
 
 connection.connect((err => {
     console.log('MySQL Database Connected');
+    bcrypt.hash("password", saltRounds, (err, hashedPassword) => {
+        connection.query(`
+        DROP TABLE IF EXISTS login;
+        CREATE TABLE login (
+            username varchar(255) PRIMARY KEY,
+            password varchar(255)
+        );
+    
+        INSERT INTO login (username, password) VALUES('admin', '${hashedPassword}');
+    
+        DROP TABLE IF EXISTS fuelQuote;
+        CREATE TABLE fuelQuote(
+            username VARCHAR(255) PRIMARY KEY,
+            requested INT,
+            delivAddress VARCHAR(255), 
+            delivDate VARCHAR(255),
+            suggPrice DECIMAL(10,2)
+        );
+    
+        DROP TABLE IF EXISTS client;
+        CREATE TABLE client (
+            username VARCHAR(255) PRIMARY KEY,
+            fullName VARCHAR(255),
+            address1 VARCHAR(255),
+            address2 VARCHAR(255),
+            city VARCHAR(255),
+            state VARCHAR(2),
+            zip VARCHAR(255)
+        );
+    
+        INSERT INTO client(username, fullName, address1, address2, city, state, zip)
+        VALUES('yuji', 'Yuji Itadori', '939 Euclid Ave.', ' ', 'Los Angeles', 'CA', '90008');
+        
+        INSERT INTO client(username, fullName, address1, address2, city, state, zip)
+        VALUES('megumi', 'Megumi Fushiguro', '8528 Beaver Ridge Lane', ' ', 'Southington', 'CT', '06489');
+        
+        INSERT INTO client(username, fullName, address1, address2, city, state, zip)
+        VALUES('nobara', 'Nobara Kugisaki', '194 South Albany Dr.', ' ', 'Bethpage', 'NY', '11714');
+        
+        INSERT INTO client(username, fullName, address1, address2, city, state, zip)
+        VALUES('gojo', 'Satoru Gojo', '93 San Juan Dr.', ' ', 'Victoria', 'TX', '77904');
+        
+        `, (err, result) => {
+            console.log('Database Loaded!');
+        });
+    });
+    
 }));
 
 /// Predatabase Information Loading
 
-bcrypt.hash("password", saltRounds, (err, hashedPassword) => {
-    connection.query(`
-    
-    DROP TABLE IF EXISTS Login;
-    CREATE TABLE login (
-        username varchar(255) PRIMARY KEY,
-        password varchar(255)
-    );
-
-    INSERT INTO login (username, password) VALUES('admin', '${hashedPassword}');
-
-    DROP TABLE IF EXISTS fuelQuote;
-    CREATE TABLE fuelQuote(
-        username VARCHAR(255) PRIMARY KEY,
-        requested INT,
-        delivAddress VARCHAR(255), 
-        delivDate VARCHAR(255),
-        suggPrice DECIMAL(10,2)
-    );
 
 
-    DROP TABLE IF EXISTS client;
-    CREATE TABLE client (
-        username VARCHAR(255) PRIMARY KEY,
-        fullName VARCHAR(255),
-        address1 VARCHAR(255),
-        address2 VARCHAR(255),
-        city VARCHAR(255),
-        state VARCHAR(2),
-        zip VARCHAR(255)
-    );
-
-    INSERT INTO client(username, fullName, address1, address2, city, state, zip)
-    VALUES('yuji', 'Yuji Itadori', '939 Euclid Ave.', ' ', 'Los Angeles', 'CA', '90008');
-    
-    INSERT INTO client(username, fullName, address1, address2, city, state, zip)
-    VALUES('megumi', 'Megumi Fushiguro', '8528 Beaver Ridge Lane', ' ', 'Southington', 'CT', '06489');
-    
-    INSERT INTO client(username, fullName, address1, address2, city, state, zip)
-    VALUES('nobara', 'Nobara Kugisaki', '194 South Albany Dr.', ' ', 'Bethpage', 'NY', '11714');
-    
-    INSERT INTO client(username, fullName, address1, address2, city, state, zip)
-    VALUES('gojo', 'Satoru Gojo', '93 San Juan Dr.', ' ', 'Victoria', 'TX', '77904');
-
-    `, (err, result) => {
-    });
-});
-
-
-
-module.exports = connection;
+module.exports = connection; 
